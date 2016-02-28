5a9b81c8f6025a7b06d94f011a58aa5ce95bc8ac
Alex Lloreda <alexlloreda@gmail.com>
2016-02-28 22:16:05 +1100

    Week2 complete

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -6,20 +6,18 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+n = size(theta);
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+[J, grad] = costFunction(theta, X, y);
 
-
-
-
+J = J + lambda/(2*m) * sum(theta(2:end).^2);
+grad = grad + lambda/m .* prepad(theta(2:end),n(1));
 
 
 % =============================================================
