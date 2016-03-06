2db26f2828a42bfb2902fec67076ac115f8fff04
Alex Lloreda <alexlloreda@gmail.com>
2016-03-06 20:04:56 +1100

    neural network prediction complete. Adding the 1s in the right way was the key for the second matrixx

--- ex3/predict.m
+++ ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -20,16 +20,14 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
-
-
-
-
-
-
-
+% If single row of X: a2 = sigmoid(Theta1 * X);  p = sigmoid(Theta2 * a2);
+a2 = sigmoid(Theta1 * X');
+a2 = [ones(size(a2,2), 1)'; a2];
+z = sigmoid(Theta2 * a2);
+[v, p] = max(z', [], 2);
 
 % =========================================================================
-
-
 end
